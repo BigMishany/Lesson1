59799e63c865f89e483b261cf22b419d20fdadf5
AWISK <noname2039523@gmail.com>
2018-06-02 15:51:39 +0300

    Finished second activations, the data is automatically output by sorted

--- app/src/main/java/xyz/bgw/mikhail/lesson1/Activity2.java
+++ app/src/main/java/xyz/bgw/mikhail/lesson1/Activity2.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class Activity2 extends AppCompatActivity {
@@ -17,14 +19,14 @@ public class Activity2 extends AppCompatActivity {
     Button buttonShow_2;
     EditText Text_user_2;
     TextView List_2;
-    TreeSet Data_2 ;
+    TreeSet Data_2 = new TreeSet<String>();
     String Data_to_string_2;
 
 
 
     public static Intent createStartIntetn_2(Context context)
     {
-        Intent intent = new Intent(context , Activity1.class);
+        Intent intent = new Intent(context , Activity2.class);
         return intent;
     }
 
@@ -33,13 +35,13 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
         buttonAdd_2 = findViewById(R.id.buttonAdd_2);
-        buttonShow_2 = findViewById(R.id.buttonShow);
-        Text_user_2 = findViewById(R.id.editText);
-        List_2 = findViewById(R.id.textView2);
+        buttonShow_2 = findViewById(R.id.buttonShow_2);
+        Text_user_2 = findViewById(R.id.editText_2);
+        List_2 = findViewById(R.id.textView_2);
         buttonAdd_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Data_2.add(Text_user_2.getText());
+                Data_2.add(Text_user_2.getText().toString());
                 List_2.setText("Add information complete");
                 Text_user_2.setText("");
             }
@@ -48,10 +50,8 @@ public class Activity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Data_to_string_2="";
-
-                    Data_to_string_2 = Data_2.toString();
-
-                List_2.setText(Data_to_string_2);
+                List_2.setText(Data_2.toString());
+                //List_2.setText(Data_to_string_2);
             }
         });
 
